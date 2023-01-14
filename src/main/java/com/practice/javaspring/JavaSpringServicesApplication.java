3a889216b8f98f05a0d0196a363fb4709d880aaa
snigdha <snigdhabiswas10@gmail.com>
2023-01-14 23:28:11 +0530

    Snigdha | Creates a Spring boot Application

---
+++ src/main/java/com/practice/javaspring/JavaSpringServicesApplication.java
@@ -0,0 +1,14 @@
+package com.practice.javaspring;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringServicesApplication.class, args);
+	}
+
+}
